a476a674fb74e8910c7a47a5aa63d6edd82b3f7a
flagos <flagospub@gmail.com>
2012-05-12 12:48:56 +0200

    add pagination

--- public/js/client_table.js
+++ public/js/client_table.js
@@ -4,8 +4,16 @@ function displayMessage(text, style) {
 	_$("message").innerHTML = "<p class='" + (style || "ok") + "'>" + text + "</p>"; 
 } 
 
+// helper function to get path of a demo image
+function image(relativePath) {
+	return "img/" + relativePath;
+}
+
 window.onload = function() {
-    editableGrid = new EditableGrid("DemoGridJSON"); 
+    editableGrid = new EditableGrid("DemoGridJSON", {
+	pageSize: 10,
+    }
+); 
     editableGrid.tableLoaded = function() { this.renderGrid("tablecontent", "testgrid"); };
     editableGrid.loadJSON("/clients.json");
     
@@ -47,4 +55,51 @@ window.onload = function() {
     // filter when something is typed into filter
     _$('filter').onkeyup = function() {editableGrid.filter(_$('filter').value); };
 
+    // update paginator whenever the table is rendered (after a sort, filter, page change, etc.)
+    editableGrid.tableRendered = function() { this.updatePaginator(); };
+
 }
+
+// function to render the paginator control
+EditableGrid.prototype.updatePaginator = function()
+{
+	var paginator = $("#paginator").empty();
+	var nbPages = this.getPageCount();
+
+	// get interval
+	var interval = this.getSlidingPageInterval(20);
+	if (interval == null) return;
+	
+	// get pages in interval (with links except for the current page)
+	var pages = this.getPagesInInterval(interval, function(pageIndex, isCurrent) {
+		if (isCurrent) return "" + (pageIndex + 1);
+		return $("<a>").css("cursor", "pointer").html(pageIndex + 1).click(function(event) { editableGrid.setPageIndex(parseInt($(this).html()) - 1); });
+	});
+		
+	// "first" link
+	var link = $("<a>").html("<img src='" + image("gofirst.png") + "'/>&nbsp;");
+	if (!this.canGoBack()) link.css({ opacity : 0.4, filter: "alpha(opacity=40)" });
+	else link.css("cursor", "pointer").click(function(event) { editableGrid.firstPage(); });
+	paginator.append(link);
+
+	// "prev" link
+	link = $("<a>").html("<img src='" + image("prev.png") + "'/>&nbsp;");
+	if (!this.canGoBack()) link.css({ opacity : 0.4, filter: "alpha(opacity=40)" });
+	else link.css("cursor", "pointer").click(function(event) { editableGrid.prevPage(); });
+	paginator.append(link);
+
+	// pages
+	for (p = 0; p < pages.length; p++) paginator.append(pages[p]).append(" | ");
+	
+	// "next" link
+	link = $("<a>").html("<img src='" + image("next.png") + "'/>&nbsp;");
+	if (!this.canGoForward()) link.css({ opacity : 0.4, filter: "alpha(opacity=40)" });
+	else link.css("cursor", "pointer").click(function(event) { editableGrid.nextPage(); });
+	paginator.append(link);
+
+	// "last" link
+	link = $("<a>").html("<img src='" + image("golast.png") + "'/>&nbsp;");
+	if (!this.canGoForward()) link.css({ opacity : 0.4, filter: "alpha(opacity=40)" });
+	else link.css("cursor", "pointer").click(function(event) { editableGrid.lastPage(); });
+	paginator.append(link);
+};
